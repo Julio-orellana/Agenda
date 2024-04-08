f721579944cd0e2ccebb11bcc0a1fb432f3f9b58
Julio Orellana <julioes134@outlook.es>
2024-04-08 01:09:21 -0600

    ACTUALIZACION DE ARCHIVOS

--- agenda.py
+++ agenda.py
@@ -14,13 +14,19 @@ def main():
         print(f"\n{date}")
         if not menu_principal():
             break  # Salir del bucle si el inicio de sesión no es exitoso
-        print('''''')# Se deben imprimir las posibles opciones de la agenda
+        print('''
+                1. Ingresar tareas a la agenda
+                2. Eliminar una tarea de la agenda
+                3. Buscar tarea por id
+                4. Listar todas las tareas de la agenda
+                5. Actualizar una tarea en la agenda
+                ''')# Se deben imprimir las posibles opciones de la agenda
         opcion = input("Ingrese una opcion: ")
         while flujo:
             if opcion == "1": #Opcion 1 ingresa una tarea a la agenda
-                fecha = input("Ingrese la fecha: ")
-                tarea = input("Ingrese la descripción de la tarea: ")
-                Agenda.ingresarTarea(fecha, tarea)
+
+                Agenda.ingresarTarea()
+                    
             else:
                 flujo = False
                 
